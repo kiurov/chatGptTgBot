b6104a6332b693e56fea647a75655bc921457cce
Konstantin Yurov <kkrd@icloud.com>
2023-04-02 23:19:03 +0300

    Model changed to gpt-3.5-turbo

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 
-telegramBotToken = '5842336746:AAG9ZTvZo-voV0m8QurgEgOwaJ0LSs-z5lI'
-openai.api_key = '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'
+telegramBotToken = '<telegramBotToken>'
+openai.api_key = '<openAiToken>'
 
 bot = Bot(telegramBotToken)
 dp = Dispatcher(bot)

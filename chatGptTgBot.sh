1c9f3370b3fd978ad28139fff4283a51ad1a4aad
Konstantin Yurov <kkrd@icloud.com>
2023-04-13 16:43:39 +0300

    Added comments

--- chatGptTgBot.sh
+++ chatGptTgBot.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# Defining the Telegram Bot Token variable in the environment
 read -p "Type your Telegram Bot Token: " tgBotToken
 export telegramBotToken=$tgBotToken
+
+# Defining the OpenAI API Key variable in the environment
 read -p "Type your OpenAI API Key: " openAiApi
 export openAiApiKey=$openAiApi
 
+# Run the bot
 python3 chatGptTgBot.py &
 
 echo

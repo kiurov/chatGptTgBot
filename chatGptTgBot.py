179278ab939d6518497f657c814f033cebf46e67
Konstantin Yurov <kkrd@icloud.com>
2023-04-23 13:59:19 +0300

    added auto restart

--- chatGptTgBot.py
+++ chatGptTgBot.py
@@ -3,19 +3,27 @@ import os
 import telebot
 import openai
 
+tockens = []
+pathToFileWithTockens = open('/home/ekonyur/tockens')
+for i in pathToFileWithTockens:
+    tockens.append(i[:-1])
+pathToFileWithTockens.close()
+
 # Define the bot token
-telegramBotToken = os.environ["telegramBotToken"]
+#telegramBotToken = os.environ["telegramBotToken"]
+telegramBotToken = tockens[0]
 
 # Create the bot instance
 bot = telebot.TeleBot(telegramBotToken)
 
 # Define the OpenAI API key
-openai.api_key = os.environ["openAiApiKey"]
+#openai.api_key = os.environ["openAiApiKey"]
+openai.api_key = tockens[1]
 
 # Define the bot's start message handler
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Hi, I am an AI-powered 🧠 bot. Ask me a question and i will do my best to answer it!\n\nПривет, я бот с искусственным 🧠 интеллектом. Задайте мне вопрос и я сделаю все возможное, чтобы ответить на него!")
+    bot.send_message(message.chat.id, "Hi, I am an AI-powered 🧠 bot. Ask me a question and I will do my best to answer it!\n\nПривет, я бот с искусственным 🧠 интеллектом. Задайте мне вопрос и я сделаю все возможное, чтобы ответить на него!")
 
 
 # Define the bot's response handler

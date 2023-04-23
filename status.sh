01358f6bfe50faae2220033c961ce75a3ab400f4
Konstantin Yurov <kkrd@icloud.com>
2023-04-23 12:12:18 +0300

    added auto restart

--- status.sh
+++ status.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ps -aef | egrep chatGptTgBot.py | grep -v grep
 if [ $? -ne 0 ]; then 
-    bash ~/chatGptTgBot/chatGptTgBot.sh -n &> /dev/null
-    processId=$(ps -aef | gawk '$NF~"^chatGptTgBot.py"{print $2,$NF}')
-    bash ~/telegramMessage/telegramMessage.sh -m "ChatGPT - KAK CHLVK was restarted! $processId"
+    bash ~/chatGptTgBot/chatGptTgBot.sh -n
+    #processId=$(ps -aef | gawk '$NF~"^chatGptTgBot.py"{print $2,$NF}')
+    bash ~/telegramMessage/telegramMessage.sh -m "ChatGPT - KAK CHLVK was restarted! $(ps -aef | gawk '$NF~"^chatGptTgBot.py"{print $2,$NF}')"
 fi

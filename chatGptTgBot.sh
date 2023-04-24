affe97007fe9631d7bd17572a1bfd3bcfccbc175
Konstantin Yurov <kkrd@icloud.com>
2023-04-24 10:55:35 +0300

    Changed path to python3

--- chatGptTgBot.sh
+++ chatGptTgBot.sh
@@ -2,19 +2,8 @@
 
 ps -aef | egrep chatGptTgBot.py | grep -v grep
 if [ $? -ne 0 ]; then 
-    # Defining the Telegram Bot Token and OpenAI API Key variables in the environment
-#    if [ "$1" == "-n"  ]; then
-#        tgBotToken=$(gawk '$1~/^tg$/{print $2}' ~/tockens)
-#        openAiApi=$(gawk '$1~/^ai$/{print $2}' ~/tockens)
-#    else
-#        read -p "Type your Telegram Bot Token: " tgBotToken
-#        read -p "Type your OpenAI API Key: " openAiApi
-#    fi
-#    export telegramBotToken=$tgBotToken
-#    export openAiApiKey=$openAiApi
-
     # Run the bot
-    python3 chatGptTgBot.py &
+    /usr/bin/python3 chatGptTgBot.py &
 
     echo
     echo "Done! Process ID is:"
